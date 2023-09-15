bd2c836392719bfb5bbceb65606b2b14f747f69e
Krystian <krystian.wojda-98@wp.pl>
2023-09-15 16:15:40 +0200

    Create components, importing components to home page

---
+++ cartrader/components/Car/Hero.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="relative h-screen w-100 bg-no-repeat bg-cover bg-center bg-[url('https://wallpaperaccess.com/full/787663.jpg')]">
+    <div class="z-10 absolute w-full h-full flex justify-center items-center">
+      <div class="text-center w-1/2">
+        <h1 class="text-8xl mb-10 text-white">Find your car</h1>
+        <CarSearchBar/>
+      </div>
+    </div>
+    <div class="z-1 absolute w-full h-full bg-black opacity-50"></div>
+  </div>
+</template>
---
+++ cartrader/components/Car/SearchBar.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="w-[1000px] text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto">
+    <input type="text" class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none" placeholder="Search by city">
+    <button class="bg-sky-600 px-10 text-white">Search</button>
+  </div>
+</template>
