4f09232c9828c9d8c809bf00e6a49e4380af1faa
Krystian <krystian.wojda-98@wp.pl>
2023-09-15 15:56:10 +0200

    Preparing file based routing

--- cartrader/app.vue
+++ cartrader/app.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
-    <!-- NAVBAR -->
-    <header class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
-      <a class="text-3xl" href="">Cartrader</a>
-    </header>
-    <!-- NAVBAR -->
-
+    <NuxtPage/>
   </div>
 </template>
 
@@ -14,5 +9,3 @@
     font-family: revert;
   }
 </style>
-<script setup lang="ts">
-</script>
