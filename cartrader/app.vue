a01b9a94ca372376c4acad829602dc1164cef6e9
Krystian <krystian.wojda-98@wp.pl>
2023-09-15 09:30:11 +0200

    Building the Home page

--- cartrader/app.vue
+++ cartrader/app.vue
@@ -2,8 +2,26 @@
   <div>
     <!-- NAVBAR -->
     <header class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
-      <a class="text-3xl font-mono" href="">Cartrader</a>
+      <a class="text-3xl" href="">Cartrader</a>
     </header>
-    <!--  -->
+    <!-- NAVBAR -->
+    <!-- HOME -->
+    <div class="relative h-screen w-100 bg-no-repeat bg-cover bg-center bg-[url('https://wallpaperaccess.com/full/787663.jpg')]">
+      <div class="z-10 absolute w-full h-full flex justify-center items-center">
+        <div class="text-center w-1/2">
+          <h1 class="text-8xl mb-10 text-white">Find your car</h1>
+        </div>
+      </div>
+      <div class="z-1 absolute w-full h-full bg-black opacity-50"></div>
+    </div>
+    <!-- HOME -->
   </div>
 </template>
+
+<style>
+  * {
+    font-family: revert;
+  }
+</style>
+<script setup lang="ts">
+</script>
