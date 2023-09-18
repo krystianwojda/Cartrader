2cba8532274bbd2b498dcfa35eca1f9f37d41b4d
Krystian <krystian.wojda-98@wp.pl>
2023-09-18 11:13:05 +0200

    Adding programatic navigation, add error if value is empty

--- cartrader/components/Car/SearchBar.vue
+++ cartrader/components/Car/SearchBar.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="w-[1000px] text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto">
-    <input type="text" class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none" placeholder="Search by city">
-    <button class="bg-sky-600 px-10 text-white">Search</button>
+    <input type="text" class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none" placeholder="Search by city" v-model="city" :class="cityError ? 'border-red-600 border' : ''">
+    <button class="bg-sky-600 px-10 text-white" @click="handleSearch">Search</button>
   </div>
 </template>
+
+<script setup>
+const city = ref('');
+const cityError = ref(false);
+
+const handleSearch = () => {
+  if (!city.value) {
+    return cityError.value = true;
+  }
+  navigateTo(`/city/${city.value}/car`)
+};
+</script>
