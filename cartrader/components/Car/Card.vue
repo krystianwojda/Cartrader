3044ec917f6f14bcc13528ef66d16797bc85d3f0
Krystian <krystian.wojda-98@wp.pl>
2023-09-18 10:39:49 +0200

    Filling the Car List Page witch components

---
+++ cartrader/components/Car/Card.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]">
+    <div class="flex h-full">
+      <img src="https://ocdn.eu/pulscms-transforms/1/ntgk9kuTURBXy8xMzdkZjNkMS0zZjg2LTQwZjctYTE1Zi02YjAzMzcxNTc5MWEuanBlZ5DeAAGhMAU" alt="" class="w-[300px] h-full">
+      <div class="p-4 flex flex-col">
+        <div>
+          <h1 class="text-2xl text-blue-700">Mercedes AMG C63 S E Perfomance</h1>
+          <p class="text-gray-700">To najpotężniejsza Klasa C w historii. Ta bestia występuje w przyrodzie jako limuzyna i kombi. Wszystko robi lepiej i szybciej. Lepiej skręca, szybciej przyspiesza, bardziej rozpieszcza luksusem</p>
+        </div>
+        <h1 class="mt-auto text-xl">Cena: 250,000 zł</h1>
+      </div>
+    </div>
+  </div>
+</template>
