ef5d6d91f0670a7ac3d026d686ac024718939cc7
Krystian <krystian.wojda-98@wp.pl>
2023-09-18 10:56:05 +0200

    Create Unique Dynamic Path for Detail Page Car

---
+++ cartrader/components/Car/Detail/Description.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="mt-5">
+    <p class="mb-10">Mercedes-AMG C 63 S E Performance pod maską skrywa zamontowany wzdłużnie 2-litrowy silnik turbo o mocy 476 KM (238 KM z 1 litra pojemności!). To najmocniejszy seryjnie produkowany czterocylindrowiec na świecie. Silnik elektryczny rozwija moc 204 KM i został umieszczony na tylnej osi, zintegrowany we wspólny moduł z elektrycznie sterowaną dwubiegową przekładnią oraz elektronicznie sterowanym tylnym mechanizmem różnicowym o ograniczonym poślizgu. Eksperci określają ten układ mianem hybrydy P3.</p>
+  </div>
+</template>
---
+++ cartrader/components/Car/Detail/Hero.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="mt-10">
+    <img src="https://ocdn.eu/pulscms-transforms/1/ntgk9kuTURBXy8xMzdkZjNkMS0zZjg2LTQwZjctYTE1Zi02YjAzMzcxNTc5MWEuanBlZ5DeAAGhMAU" class="w-full" alt="">
+    <h1 class="mt-10 text-4xl">Mercedes AMG C63 S E Perfomance</h1>
+    <div class="text-slate-500 flex text-lg mt-3 border-b pb-5 justify-between">
+      <div class="flex">
+        <p class="mr-2">2.0 476KM</p>
+        <p class="mr-2">|</p>
+        <p class="mr-2">94,421km</p>
+      </div>
+      <div>
+        <p class="font-bold text-2xl">Cena: 250,000 zł</p>
+      </div>
+    </div>
+  </div>
+</template>
