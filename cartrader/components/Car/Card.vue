8cfde112d097f1a7f2f99ce637f8de1ca00ed6cd
Krystian <krystian.wojda-98@wp.pl>
2023-09-20 14:00:18 +0200

    Dynamically Rendering the Car Cards

--- cartrader/components/Car/Card.vue
+++ cartrader/components/Car/Card.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]">
+  <div class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]" @click="navigateTo(`/car/${car.name}-${car.id}`)">
     <div class="flex h-full">
-      <img src="https://ocdn.eu/pulscms-transforms/1/ntgk9kuTURBXy8xMzdkZjNkMS0zZjg2LTQwZjctYTE1Zi02YjAzMzcxNTc5MWEuanBlZ5DeAAGhMAU" alt="" class="w-[300px] h-full">
+      <img :src="car.url" alt="" class="w-[300px] h-full">
       <div class="p-4 flex flex-col">
         <div>
-          <h1 class="text-2xl text-blue-700">Mercedes AMG C63 S E Perfomance</h1>
-          <p class="text-gray-700">To najpotężniejsza Klasa C w historii. Ta bestia występuje w przyrodzie jako limuzyna i kombi. Wszystko robi lepiej i szybciej. Lepiej skręca, szybciej przyspiesza, bardziej rozpieszcza luksusem</p>
+          <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
+          <p class="text-gray-700">{{ car.description }}</p>
         </div>
-        <h1 class="mt-auto text-xl">Cena: 250,000 zł</h1>
+        <h1 class="mt-auto text-xl">{{ car.price}} zł</h1>
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+  const props = defineProps({
+    car: Object
+  });
+</script>
--- cartrader/components/Car/Cards.vue
+++ cartrader/components/Car/Cards.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="w-full">
-    <CarCard/>
+    <CarCard v-for="car in data" :key="car.id" :car="car"/>
   </div>
 </template>
+
+<script setup>
+  import cars from '@/data/cars.json';
+  
+  const data = cars;
+</script>
