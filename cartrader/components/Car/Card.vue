1b6a900e917c8091d92e6c34b8dc35c8ed48ce12
Krystian <krystian.wojda-98@wp.pl>
2023-09-22 16:19:26 +0200

    Add button to show detail

--- cartrader/components/Car/Card.vue
+++ cartrader/components/Car/Card.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]">
     <div class="flex h-full">
-      <img :src="car.url" alt="" class="w-[300px] h-full" @click="navigateTo(`/car/${car.name}-${car.id}`)">
+      <img :src="car.url" alt="" class="w-[300px] h-full">
       <div class="p-4 flex flex-col">
         <div class="flex justify-between">
           <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
@@ -9,6 +9,7 @@
         </div>
         <p class="text-gray-700">{{ car.description }}</p>
         <h1 class="mt-auto text-xl">{{ car.price}} zł</h1>
+        <button class="bg-blue-400 mt-1 mb-1 p-2" @click="navigateTo(`/car/${car.name}-${car.id}`)">Show detail</button>
       </div>
     </div>
   </div>
