21c633b21103c42bd70f59cb6cd7d4c83add833a
Krystian <krystian.wojda-98@wp.pl>
2023-09-25 12:53:59 +0200

    Adding the Car Make Filter

--- cartrader/components/Car/SideBar.vue
+++ cartrader/components/Car/SideBar.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="shadow border w-64 mr-10 z-30 h-[190px]">
+    <!-- LOCATION START -->
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Location</h3>
       <h3 @click="updateModal('location')" class="text-blue-400 capitalize">{{ route.params.city }}</h3>
@@ -8,18 +9,30 @@
         <button class="bg-blue-400 w-full mt-2 rounded text-white p-1" @click="onChangeLocation">Apply</button>
       </div>
     </div>
+    <!-- LOCATION END -->
+    <!-- MAKE START -->
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Make</h3>
-      <h3 class="text-blue-400 capitalize">Toyota</h3>
+      <h3 class="text-blue-400 capitalize" @click="updateModal('make')">{{ route.params.make || "Any" }}</h3>
+      <div class="absolute border shadow left-56 p-5 top-1 -m-1 w-[600px] flex justify-between flex-wrap bg-white" v-if="modal.make">
+        <h4 v-for="make in makes" :key="make" class="w-1/3" @click="onChangeMake(make)">{{ make }}</h4>
+      </div>
     </div>
+    <!-- MAKE END -->
+    <!-- PRICE START -->
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Price</h3>
       <h3 class="text-blue-400 capitalize">Any</h3>
     </div>
+    <!-- PRICE END -->
   </div>
 </template>
 
 <script setup>
+import make from '@/data/makes.json';
+
+const makes = make;
+
 const modal = ref({
   make: false,
   location: false,
@@ -45,5 +58,10 @@ const onChangeLocation = () => {
   updateModal('location');
   navigateTo(`/city/${city.value}/car/${route.params.make}`);
   city.value = '';
+};
+
+const onChangeMake = (make) => {
+  updateModal('make');
+  navigateTo(`/city/${route.params.city}/car/${make}`);
 };
 </script>
