e9abe236116698f8fe1eb842c547954485cd317c
Krystian <krystian.wojda-98@wp.pl>
2023-09-25 16:54:34 +0200

    Creating admin page witch all your products

---
+++ cartrader/components/Car/ListingCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="shadow rounded overflow-hidden flex justify-between mb-4">
+    <div class="flex">
+      <img :src="listing.url" :alt="listing.name" class="w-80 mr-3 h-44">
+      <div class="p-3">
+        <h1 class="text-2xl">{{ listing.name }}</h1>
+        <p class="text-blue-400">{{ listing.price }} zł</p>
+      </div>
+    </div>
+    <div class="p-3 flex">
+      <NuxtLink class="text-blue-400 mr-4" :to="`/profile/listing/view/${listing.id}`">View</NuxtLink>
+      <p class="text-red-400 cursor-pointer">Delete</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  listing: Object
+})
+</script>
---
+++ cartrader/pages/profile/listings/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
+    <div class="flex justify-between mt-24 items-center">
+      <h1 class="text-6xl">My Listings</h1>
+      <NuxtLink to="/profile/listings/create" class="w-9 h-9 bg-blue-400 flex justify-center items-center rounded-full text-white font-bold cursor-pointer">+</NuxtLink>
+    </div>
+    <div class="shadow rounded p-3 mt-5">
+      <CarListingCard v-for="listing in listings" :key="listing" :listing="listing"/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import listing from '@/data/listings.json';
+
+const listings = listing;
+</script>
