e5a1287ff191ae14a79b99101ae8d5c24ca80218
Krystian <krystian.wojda-98@wp.pl>
2023-09-26 10:21:31 +0200

    Create form, pass array with model car to input select

---
+++ cartrader/pages/profile/listings/view/create.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
+    <div class="mt-24">
+      <h1 class="text-6xl mt-24">Create a New Listing</h1>
+    </div>
+    <div class="shadow rounded p-3 mt-5 flex flex-wrap justify-between">
+      <CarAdSelect title="Make *" :options="makes" name="make" @change-input="onChangeInput"/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import dataMake from '@/data/makes.json';
+
+const makes = dataMake;
+const info = useState('adInfo', () => {
+  return {
+    make: '',
+    model: '',
+    year: '',
+    miles: '',
+    price: '',
+    city: '',
+    seats: '',
+    description: '',
+    image: null
+  }
+});
+
+const onChangeInput = (data, name) => {
+  info.value[name] = data;
+};
+</script>
