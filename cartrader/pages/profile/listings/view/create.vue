789136fc2e5f0f3f82875aa491f4e18fae5c07f0
Krystian <krystian.wojda-98@wp.pl>
2023-09-26 12:39:20 +0200

    Create form, downloading data from the inputs

--- cartrader/pages/profile/listings/view/create.vue
+++ cartrader/pages/profile/listings/view/create.vue
@@ -5,6 +5,7 @@
     </div>
     <div class="shadow rounded p-3 mt-5 flex flex-wrap justify-between">
       <CarAdSelect title="Make *" :options="makes" name="make" @change-input="onChangeInput"/>
+      <CarAdInput v-for="input in inputs" :key="input.id" :title="input.title" :name="input.name" :placeholder="input.placeholder" @change-input="onChangeInput"/>
     </div>
   </div>
 </template>
@@ -22,6 +23,7 @@ const info = useState('adInfo', () => {
     price: '',
     city: '',
     seats: '',
+    features: '',
     description: '',
     image: null
   }
@@ -30,4 +32,43 @@ const info = useState('adInfo', () => {
 const onChangeInput = (data, name) => {
   info.value[name] = data;
 };
+
+const inputs = [
+  {
+    id: 1,
+    title: 'Model *',
+    name: 'model',
+    placeholder: "Civic"
+  },
+  {
+    id: 1,
+    title: 'Year *',
+    name: 'year',
+    placeholder: "2019"
+  },
+  {
+    id: 3,
+    title: 'Miles *',
+    name: 'Miles',
+    placeholder: "22182"
+  },
+  {
+    id: 4,
+    title: 'City *',
+    name: 'city',
+    placeholder: "Oklahoma"
+  },
+  {
+    id: 5,
+    title: 'Number of Seats *',
+    name: 'seats',
+    placeholder: "5"
+  },
+  {
+    id: 6,
+    title: 'Features *',
+    name: 'features',
+    placeholder: "Leather Interior, No Accidents"
+  },
+];
 </script>
