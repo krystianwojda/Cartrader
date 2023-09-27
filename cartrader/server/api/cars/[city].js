54e76a0b3d28ef747598bf2b8265da60831ff5f6
Krystian <krystian.wojda-98@wp.pl>
2023-09-27 16:36:04 +0200

    Extracting the Path Parameters

--- cartrader/server/api/cars/[city].js
+++ cartrader/server/api/cars/[city].js
@@ -1,4 +1,11 @@
 import cars from '@/data/cars.json';
 export default defineEventHandler((event) => {
+    const {city} = event.context.params;
+
+    let filteredCars = cars;
+
+    filteredCars = filteredCars.filter(car => {
+        return car.city.toLowerCase() === city.toLowerCase();
+    })
     return cars;
 });
