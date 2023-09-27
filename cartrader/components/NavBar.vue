c196ea845795cfae2e7cef39d189aac03f512455
Krystian <krystian.wojda-98@wp.pl>
2023-09-27 11:18:49 +0200

    Conditionally rendering UI on auth status

--- cartrader/components/NavBar.vue
+++ cartrader/components/NavBar.vue
@@ -1,5 +1,14 @@
 <template>
   <header class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
     <NuxtLink class="text-3xl" to="/">Cartrader</NuxtLink>
+    <div v-if="user" class="flex">
+      <NuxtLink to="/profile/listings" class="mr-5">Profile</NuxtLink>
+      <p class="cursor-pointer">Logout</p>
+    </div>
+    <NuxtLink v-else to="/login">Login</NuxtLink>
   </header>
 </template>
+
+<script setup>
+const user = useSupabaseUser();
+</script>
