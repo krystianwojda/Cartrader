170530eb7ed0a30ab60d2c76a8f69521fa85ff35
Krystian <krystian.wojda-98@wp.pl>
2023-10-03 16:42:30 +0200

    Fix bug in links to image

--- cartrader/components/Car/Card.vue
+++ cartrader/components/Car/Card.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]">
     <div class="flex h-full">
-      <img :src="`${config.public.supabase.url}/storage/v1/object/public/images/public/${car.image}`" alt="" class="w-[300px] h-full">
+      <img :src="`${config.public.supabase.url}/storage/v1/object/public/images/${car.image}`" :alt="car.name" class="w-[300px] h-full">
       <div class="p-4 flex flex-col">
         <div class="flex justify-between">
           <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
