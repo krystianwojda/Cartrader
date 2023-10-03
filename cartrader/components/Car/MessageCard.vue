ef568d201eb914251bf1a23db3e7b186fbeac07d
Krystian <krystian.wojda-98@wp.pl>
2023-10-03 12:56:25 +0200

    GET Message Endpoint

--- cartrader/components/Car/MessageCard.vue
+++ cartrader/components/Car/MessageCard.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="even:bg-gray-100 p-4">
-    <h1 class="font-bold text-2xl">Name Surname</h1>
+    <h1 class="font-bold text-2xl">{{ message.name }}</h1>
     <p class="text-gray-700">
-      Lorem ipsum
+      {{ message.message }}
     </p>
     <div class="flex items-center mt-4">
-      <p class="text-green-700 mr-10">e-mail</p>
-      <p class="text-blue-500">500-111-111</p>
+      <p class="text-green-700 mr-10">{{ message.email }}</p>
+      <p class="text-blue-500">{{ message.phone }}</p>
     </div>
   </div>
 </template>
+
+<script setup>
+const props = defineProps({
+  message: Object
+})
+</script>
--- cartrader/pages/profile/listings/view/[id].vue
+++ cartrader/pages/profile/listings/view/[id].vue
@@ -1,8 +1,12 @@
 <template>
   <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
     <div class="rounded shadow mt-20">
-      <CarMessageCard/>
-      <CarMessageCard/>
+      <CarMessageCard v-for="message in messages" :key="message.id" :message="message"/>
     </div>
   </div>
 </template>
+
+<script setup>
+const route = useRoute();
+const {data: messages} = useFetch(`/api/car/listings/${route.params.id}/message`);
+</script>
